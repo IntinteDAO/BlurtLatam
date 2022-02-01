cb9f20cb8f72a18d1367ff3945e114a70befab1d
Balvinder Singh <balvinder.singh@de-haze.com>
2022-02-01 16:13:51 +0530

    react dropzone updated

--- src/server/utils/StatsLoggerClient.js
+++ src/server/utils/StatsLoggerClient.js
@@ -18,6 +18,7 @@ export default class StatsLoggerClient {
             // Implement debug loggers here, as any new calls are added in methods below.
             this.SDC = {
                 timing() {
+                    // eslint-disable-next-line prefer-rest-params
                     console.log('StatsLoggerClient call: ', arguments);
                 }
             };
